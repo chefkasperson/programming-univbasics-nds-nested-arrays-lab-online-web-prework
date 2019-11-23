955fc1dcd582d1bf25e1fe6abae21362882f7138
Erik Kasperson <chefkasperson@gmail.com>
2019-11-23 06:23:45 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -40,6 +40,8 @@ end
 
 def matrix_update(matrix, row, column, new_value)
   matrix[0][2] = "Tiny Ewell"
+  matrix
+  
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
 end
