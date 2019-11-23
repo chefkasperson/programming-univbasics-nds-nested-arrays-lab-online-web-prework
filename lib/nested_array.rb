4b76d242b07bd210b81203747929d21b3347bfd9
Erik Kasperson <chefkasperson@gmail.com>
2019-11-23 06:09:33 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -29,7 +29,15 @@ def array_literal_matrix
   "Pat M.",
   "Kate Gompert",
   "Bruce Green"
-    ]
+    ],
+    [
+       "Hal Incandenza",
+  "Lyle",
+  "Gerhard Schtitt",
+  "Mario Incandenza",
+  "Michael Pemulis"
+]
+      ]
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
