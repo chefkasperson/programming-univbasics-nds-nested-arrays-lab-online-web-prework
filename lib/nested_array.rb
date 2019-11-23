c62d511d1d8807ee7341a7420894ea58a5a08b3e
Erik Kasperson <chefkasperson@gmail.com>
2019-11-23 06:12:50 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -38,7 +38,7 @@ def array_literal_matrix
   "Michael Pemulis"
 ]
       ]
-    nested_array.sort
+    nested_array.sort[0]
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
